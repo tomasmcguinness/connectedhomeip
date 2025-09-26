9d74027576dc0e8c1735015bec14edcb57dac43f
Tomas McGuinness <tomas@tomasmcguinness.com>
2025-09-26 15:49:21 +0100

    WIP Trying to get atomic request working

--- src/python_testing/TC_TSTAT_4_4.py
+++ src/python_testing/TC_TSTAT_4_4.py
@@ -34,6 +34,7 @@
 #     quiet: true
 # === END CI TEST ARGUMENTS ===
 
+import copy
 import asyncio
 import logging
 import random
@@ -43,6 +44,8 @@ from mobly import asserts
 
 import matter.clusters as Clusters
 from matter import ChipDeviceCtrl  # Needed before matter.FabricAdmin
+from matter.clusters import Globals
+from matter.clusters.Types import NullValue
 from matter.interaction_model import InteractionModelError, Status
 from matter.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 
@@ -51,6 +54,16 @@ logger = logging.getLogger(__name__)
 cluster = Clusters.Thermostat
 
 class TC_TSTAT_4_4(MatterBaseTest):
+
+    def make_schedule(self, systemMode, scheduleHandle=NullValue, builtIn=False):
+        schedule = cluster.Structs.ScheduleStruct(scheduleHandle=scheduleHandle, systemMode=systemMode, transitions=[], builtIn=builtIn)
+        #if self.check_pics("TSTAT.S.F00"):
+        #    preset.heatingSetpoint = heatSetpoint
+        #if self.check_pics("TSTAT.S.F01"):
+        #    preset.coolingSetpoint = coolSetpoint
+        #if name is not None:
+        #    preset.name = name
+        return schedule
 
     async def write_schedules(self,
                               endpoint,
@@ -63,6 +76,28 @@ class TC_TSTAT_4_4(MatterBaseTest):
         status = result[0].Status
         asserts.assert_equal(status, expected_status, f"Schedules write returned {status.name}; expected {expected_status.name}")
         return status
+
+    async def send_atomic_request_begin_command(self,
+                                                dev_ctrl: ChipDeviceCtrl = None,
+                                                endpoint: int = None,
+                                                timeout: int = 1800,
+                                                expected_status: Status = Status.Success,
+                                                expected_overall_status: Status = Status.Success,
+                                                expected_preset_status: Status = Status.Success,
+                                                expected_schedules_status: Status = None,
+                                                expected_timeout: int = None):
+        try:
+            response = await self.send_single_cmd(cmd=cluster.Commands.AtomicRequest(requestType=Globals.Enums.AtomicRequestTypeEnum.kBeginWrite,
+                                                                                     attributeRequests=[
+                                                                                         cluster.Attributes.Schedules.attribute_id],
+                                                                                     timeout=timeout),
+                                                  dev_ctrl=dev_ctrl,
+                                                  endpoint=endpoint)
+            self.check_atomic_response(response, expected_status, expected_overall_status,
+                                       expected_preset_status, expected_schedules_status, expected_timeout)
+
+        except InteractionModelError as e:
+            asserts.assert_equal(e.status, expected_status, "Unexpected error returned")
 
     def desc_TC_TSTAT_4_4(self) -> str:
         """Returns a description of this test"""
@@ -78,8 +113,12 @@ class TC_TSTAT_4_4(MatterBaseTest):
                      is_commissioning=True),
             TestStep("2", "TH writes to the Schedules attribute without calling the AtomicRequest command",
                      "Verify that the write request returns INVALID_IN_STATE error since the client didn't send a request to edit the schedules by calling AtomicRequest command."),                     
-            TestStep("3", "TH reads the ScheduleTypes attribute and saves it in a SupportedScheduleTypes variable.",
-                     "Verify that the read returned a list of schedule types with count >=2."),
+            TestStep("3", "TH writes to the Schedules attribute after calling the AtomicRequest begin command and then calls AtomicRequest rollback",
+                     "Verify that the Schedules attribute was not updated since AtomicRequest rollback command was called."),
+            # TestStep("4", "TH writes to the Schedules attribute after calling the AtomicRequest begin command and calls AtomicRequest commit",
+            #          "Verify that the Schedules attribute was updated with new schedules."),
+            # TestStep("5", "TH reads the ScheduleTypes attribute and saves it in a SupportedScheduleTypes variable.",
+            #          "Verify that the read returned a list of schedule types with count >=2."),
         ]
 
         return steps
@@ -106,10 +145,47 @@ class TC_TSTAT_4_4(MatterBaseTest):
         current_schedules = await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=cluster.Attributes.Schedules)
         logger.info(f"Rx'd Schedules: {current_schedules}")
 
+        scheduleCounts = len(current_schedules)
+
         # Write to the presets attribute without calling AtomicRequest command
         await self.write_schedules(endpoint=endpoint, schedules=current_schedules, expected_status=Status.InvalidInState)
 
         self.step("3")
+        #if self.pics_guard(self.check_pics("TSTAT.S.F08") and self.check_pics("TSTAT.S.A0050") and self.check_pics("TSTAT.S.Cfe.Rsp")):
+
+        #availableMode = self.get_available_scenario(presetTypes=presetTypes, presetScenarioCounts=presetScenarioCounts)
+
+        #if availableScenario is not None and len(current_presets) < numberOfPresetsSupported:
+
+        # Set the preset builtIn fields to a null built-in value
+        test_schedules = copy.deepcopy(current_schedules)
+        for schedule in test_schedules:
+            schedule.builtIn = NullValue
+
+        test_schedules.append(self.make_schedule(Clusters.Objects.Thermostat.Enums.SystemModeEnum.kHeat))
+
+        await self.send_atomic_request_begin_command()
+
+        # Write to the presets attribute after calling AtomicRequest command
+        status = await self.write_schedules(endpoint=endpoint, presets=test_schedules)
+        status_ok = (status == Status.Success)
+        asserts.assert_true(status_ok, "Presets write did not return Success as expected")
+
+        # Read the presets attribute and verify it was updated by the write
+        saved_schedules = await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=cluster.Attributes.Schedules)
+        logger.info(f"Rx'd Schedules: {saved_presets}")
+        self.check_returned_presets(test_presets, saved_presets)
+
+        await self.send_atomic_request_rollback_command()
+
+        # Read the presets attribute and verify it has been properly rolled back
+        schedules = await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=cluster.Attributes.Schedules)
+        asserts.assert_equal(schedules, current_schedules, "Presets were updated which is not expected")
+    # else:
+    #     logger.info(
+    #         "Couldn't run test step 3 since there was no available preset scenario to append")
+
+        self.step("5")
         #if self.pics_guard(self.check_pics("TSTAT.S.F0a")):
             # TH reads the ScheduleTypes attribute and saves it in a SupportedScheduleTypes variable.
         supported_schedule_types = await self.read_single_attribute_check_success(endpoint=1, cluster=cluster, attribute=cluster.Attributes.ScheduleTypes)
